f39a50ed885e4b314cc670bc041c3a1355e61ad1
wusunyu <1419924657@qq.com>
2020-04-08 21:01:49 +0800

    zhangsan-3

--- src/views/Center.vue
+++ src/views/Center.vue
@@ -9,6 +9,7 @@
 //li si zai master shang xiu gai wenjian 
 export default {
     //詹三修改的master wen jian 
+    //zhang san di er ci xiugai 
     beforeRouteEnter (to, from, next) {
         console.log(this,'center---beforerouterenter')
         next()
